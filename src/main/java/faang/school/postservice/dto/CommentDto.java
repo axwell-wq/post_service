30b6b128202cd08fc69f694f41ad138dc9bc2804
axwell-wq <Stepan07mail@gmail.com>
2025-04-14 10:53:12 +0300

    осталось проверить через постман

---
+++ src/main/java/faang/school/postservice/dto/CommentDto.java
@@ -0,0 +1,18 @@
+package faang.school.postservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDto {
+
+    @NotNull
+    private Long id;
+
+    private String content;
+    private Long authorId;
+}
---
+++ src/main/java/faang/school/postservice/mapper/CommentMapper.java
@@ -0,0 +1,14 @@
+package faang.school.postservice.mapper;
+
+import faang.school.postservice.dto.CommentDto;
+import faang.school.postservice.model.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface CommentMapper {
+
+    CommentDto toDto(Comment comment);
+
+    Comment toEntity(CommentDto commentDto);
+}
