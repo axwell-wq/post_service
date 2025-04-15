96f6982bf6d7c4af6665a76caec77b5471563641
axwell-wq <Stepan07mail@gmail.com>
2025-04-15 18:35:02 +0300

    проверил все через постмен, задача выполнена

--- src/main/java/faang/school/postservice/controller/CommentController.java
+++ src/main/java/faang/school/postservice/controller/CommentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/comment")
+@RequestMapping("api/v1/comment")
 public class CommentController {
 
     private final CommentService commentService;
--- src/main/java/faang/school/postservice/dto/CommentDto.java
+++ src/main/java/faang/school/postservice/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package faang.school.postservice.dto;
 
+import faang.school.postservice.model.Post;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,4 +16,5 @@ public class CommentDto {
 
     private String content;
     private Long authorId;
+    private Long postId;
 }
