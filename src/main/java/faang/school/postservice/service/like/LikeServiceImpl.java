2dad5c396c45592f01b1865ed77d09ff19c98e80
axwell-wq <Stepan07mail@gmail.com>
2025-04-16 13:19:46 +0300

    написал все методы, осталось проверить

--- src/main/java/faang/school/postservice/service/like/LikeServiceImpl.java
+++ src/main/java/faang/school/postservice/service/like/LikeServiceImpl.java
@@ -2,8 +2,10 @@ package faang.school.postservice.service.like;
 
 import faang.school.postservice.dto.LikeDto;
 import faang.school.postservice.mapper.LikeMapper;
+import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Like;
 import faang.school.postservice.model.Post;
+import faang.school.postservice.repository.CommentRepository;
 import faang.school.postservice.repository.LikeRepository;
 import faang.school.postservice.repository.PostRepository;
 import faang.school.postservice.service.LikeService;
@@ -18,6 +20,7 @@ public class LikeServiceImpl implements LikeService {
     private final LikeRepository likeRepository;
     private final LikeMapper likeMapper;
     private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
     public void likePost(LikeDto likeDto) {
         Post post = postRepository.findById(likeDto.getPostId()).orElseThrow(
@@ -35,10 +38,28 @@ public class LikeServiceImpl implements LikeService {
         likeRepository.save(like);
     }
 
-    public void deleteLike(LikeDto likeDto) {
-        Like like = likeRepository.findById(likeDto.getPostId()).orElseThrow(
-                () -> new EntityNotFoundException("Нет лайка на этом посте"));
-
-        likeRepository.delete(like);
+    public void deleteLikePost(LikeDto likeDto) {
+        likeRepository.deleteByPostIdAndUserId(likeDto.getPostId(), likeDto.getUserId());
     }
+
+    public void likeComment(LikeDto likeDto) {
+        Comment comment = commentRepository.findById(likeDto.getCommentId()).orElseThrow(
+                () -> new EntityNotFoundException("Нет такого комментария"));
+
+        Like existComment = likeRepository.findByCommentIdAndUserId(likeDto.getCommentId(),
+                likeDto.getUserId()).orElse(null);
+
+        if(existComment != null) {
+            throw new EntityNotFoundException("Вы уже поставили лайк");
+        }
+        Like like = likeMapper.toEntity(likeDto);
+        like.setComment(comment);
+        likeRepository.save(like);
+    }
+
+    public void deleteLikeComment(LikeDto likeDto) {
+        likeRepository.deleteByCommentIdAndUserId(likeDto.getCommentId(), likeDto.getUserId());
+    }
+
+
 }
