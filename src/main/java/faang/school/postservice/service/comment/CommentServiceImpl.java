c9c553a6fe1a91994ba8369d05bdbba385c40530
axwell-wq <Stepan07mail@gmail.com>
2025-04-19 09:03:16 +0300

    gukuk

--- src/main/java/faang/school/postservice/service/comment/CommentServiceImpl.java
+++ src/main/java/faang/school/postservice/service/comment/CommentServiceImpl.java
@@ -39,7 +39,7 @@ public class CommentServiceImpl implements CommentService {
         Comment comment = commentMapper.toEntity(commentDto);
         post.getComments().add(comment);
 
-
+        
 
         postRepository.save(post);
 
